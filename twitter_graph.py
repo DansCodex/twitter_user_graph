f88119361fd7ad6dd1f1489f8f7bf861ce552e95
DansCodex <danscodex@gmail.com>
2021-03-15 22:13:45 +0000

    Created first python in VS code

--- twitter_graph.py
+++ twitter_graph.py
@@ -1,2 +1,8 @@
 # Create a graph of a Twitter user's community 
 # Who are they following? Who follows them? Who follows each other?
+
+import os 
+
+consumer_key = os.environ.get("TWITTER_CONSUMER_KEY")
+
+print(consumer_key)
